83cc40e708bb252ff77d42326a6914e4b5845c42
Orbyfied <officialglitchout@gmail.com>
2022-03-18 18:49:18 +0100

    a

---
+++ core/src/main/java/com/github/orbyfied/carbon/content/pack/CopyAssetBuilder.java
@@ -0,0 +1,38 @@
+package com.github.orbyfied.carbon.content.pack;
+
+import com.github.orbyfied.carbon.util.resource.ResourceHandle;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+
+public class CopyAssetBuilder extends PackAssetBuilder {
+
+    private ResourceHandle source;
+
+    public CopyAssetBuilder(ResourcePackBuilder parent, PackResource loc) {
+        super(parent, loc);
+    }
+
+    public CopyAssetBuilder setSource(ResourceHandle handle) {
+        this.source = handle;
+        return this;
+    }
+
+    public ResourceHandle getSource() {
+        return source;
+    }
+
+    @Override
+    public void write(OutputStream out) throws IOException {
+        InputStream in = source.in();
+        in.transferTo(out);
+        in.close();
+    }
+
+    @Override
+    public void read(InputStream in) throws IOException {
+
+    }
+
+}
